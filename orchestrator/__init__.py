7cb63da18a88fff131c3419099c93414dad7d9c8
harishmohan60 <harish.msl@hotmail.com>
2022-04-25 17:47:51 +1000

    debugging

--- orchestrator/__init__.py
+++ orchestrator/__init__.py
@@ -10,12 +10,12 @@ def orchestrator_function(context: df.DurableOrchestrationContext):
     
     querystring_list= yield context.call_activity('combine_company_list', "None")
    
-    auto_complete_activity= [ 
-        context.call_activity('auto_complete', querystring) 
-            for querystring in querystring_list if querystring]
+    #auto_complete_activity= [ 
+        #context.call_activity('auto_complete', querystring) 
+            #for querystring in querystring_list if querystring]
     
-    inMemory_data= yield context.task_all(auto_complete_activity)
+    #inMemory_data= yield context.task_all(auto_complete_activity)
 
-    return fm.auto_complete_mover_out(inMemory_data)
+    return querystring_list #fm.auto_complete_mover_out(inMemory_data)
 
 main = df.Orchestrator.create(orchestrator_function)
